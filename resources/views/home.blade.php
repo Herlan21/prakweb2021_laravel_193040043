4dbcd868719a02177ce44cd69fcf329a1237e476
Herlan Nurachman <herlan.n9d@gmail.com>
2021-10-09 22:40:16 +0700

    Blade Templating Engine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU Blog | Home</title>
-</head>
-<body>
-    <h1>Halaman Home</h1>
+@extends('layouts.main')
 
-    <script src="js/script.js"></script>
-</body>
-</html>
+@section('container')
+    <h1>Halaman Home</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,59 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('Home', [
+    "title" => "Home"]);
 });
 
 Route::get('/about', function () {
     return view('About', [
+        "title" => "About",
         "name" => "Herlan Nurachman",
         "email" => "herlannurachman2000@gmail.com",
         "image" => "gambar1.png"
     ]);
 }); 
 
+
+
+
+
 Route::get('/blog', function () {
-    return view('Post');
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Herlan Nurachman",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias repellat voluptates sint aliquid rerum. Culpa repudiandae earum ab officia ullam vel suscipit blanditiis voluptates aut facilis numquam, laudantium omnis quas, veritatis voluptatem architecto pariatur nisi quos alias, quidem minima exercitationem. Omnis itaque est, aperiam recusandae quos quam dolorum deleniti facere ab error voluptatibus. Iste voluptatum nesciunt temporibus repellat quae ea nemo? Sunt repellat vel architecto eius expedita, dolorum excepturi beatae, earum rerum cumque necessitatibus hic doloremque ratione provident veritatis totam."
+        ],
+    
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Herlan Unpas",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus laudantium alias accusamus, neque dicta officia autem sit quisquam ipsum veniam culpa, cupiditate illo? Aut iusto autem optio modi nesciunt sequi voluptatum officiis, dolores similique nulla debitis a ad odit repudiandae voluptates at nobis repellendus error libero laboriosam vitae aliquam cum. Dolor odit temporibus quae non rerum, minima quo perspiciatis et, enim eum libero ad veritatis. Et itaque quasi nobis dolorum, consectetur magnam nostrum, excepturi, qui esse voluptates mollitia ullam. Aperiam quo ipsa non. Expedita odit quae aut eveniet accusantium dolorum numquam dolor, dolore maxime eius odio fugiat. Perferendis, qui porro."
+        ],
+    ];
+    
+
+    return view('posts',[ 
+        "title" => "Posts", 
+        "posts" => $blog_posts
+  ]);
 }); 
+
+$new_post = [];
+foreach($blog_posts as $post) {
+    if($post["slug"]=== $slug) {
+        $new_post = $post;
+    }
+}
+
+// Single Post
+
+Route::get('posts/{slug}', function($slug){
+    return view('post',[
+        "title" => "Single Post" 
+    ]);
+});
