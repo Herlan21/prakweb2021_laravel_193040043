205f73db1082cf99dd520b73c330bb22b00fa9cc
Herlan Nurachman <herlan.n9d@gmail.com>
2021-10-20 12:55:55 +0700

    Menyelesaikan Post category & Eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
 
     <h1>{{ $post->title }}</h1>
+
+    <p>By. Herlan Nurachman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     
     {!! $post->body !!}
 
