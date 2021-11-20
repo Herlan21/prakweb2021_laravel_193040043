d49d6432bbfe9c1f670a8eeb2d2cb5663baae344
Herlan Nurachman <herlan.n9d@gmail.com>
2021-11-20 08:04:21 +0700

    Menyelesaikan User login & Middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,18 +11,35 @@
           </div>
             @endif
 
+          @if(session()->has('loginError'))
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+            @endif
+
             <main class="form-signin">
                 {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
                 <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
-              <form>
-            
+              
+              <form action="/login" method="post">
+                @csrf
+
                 <div class="form-floating mb-3 mt-4">
-                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                  <label for="floatingInput">Email address</label>
+                  <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                  <label for="email">Email address</label>
+                  
+                  @error('email')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                  @enderror
                 </div>
+
+
                 <div class="form-floating">
-                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                  <label for="floatingPassword">Password</label>
+                  <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                  <label for="password">Password</label>
                 </div>
             
                 {{-- <div class="checkbox mb-3">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,12 +20,32 @@
           </li>
         </ul>
         
+        
+        
         <ul class="navbar-nav ms-auto">
+          @auth
+      
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome Back ! {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+
+              <form action="/logout" method="post">
+                @csrf
+                <button type="submit" class="dropdown-item"><i class="bi bi-caret-left-fill text-center"></i> Sign Out</button>
+              </form>
+          </li>
+
+          @else    
           <li class="nav-item">
             <a href="/login" class="nav-link {{ $active === "login" ? 'active' : ''}}"><i class="bi bi-hand-index"></i> Sign in</a>
             
           </li>
-       </ul>
+          @endauth
+        </ul>
       
       </div>
     </div>
